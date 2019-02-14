42d92f7e41b2f660a0cb412e53e93375f609f4af
Sebastian Orellana <p.sebastianorellana@gmail.com>
2019-02-14 14:01:07 -0500

    implemented DLL functions
    indexOf, remove, displayBackward, insertAfter

--- dataStructures/LinkedLists/DoublyLinkedList/doublyLinkedList.js
+++ dataStructures/LinkedLists/DoublyLinkedList/doublyLinkedList.js
@@ -73,14 +73,95 @@ class DLL {
 
   deleteLast(){
     //delete element at the end of the list
+      if(this.isEmpty()){
+          throw "List is empty. Cannot delete first element of an empty list."
+      } else {
+          if(this.tail.prev === null && this.tail.next === null){
+              //there is only one element, NOTE: I could also check the length of the list
+              this.tail = null;
+              this.head = null;
+          } else {
+                this.tail = this.tail.prev; //set new tail
+                this.tail.next = null; // remove last
+          }
+          this.length--;
+      }
   }
 
-  insertAfter(){
-    //insert and element after an item of the list
+  insertAfter(prevElement, element){
+    //insert an element after an item of the list
+      let newNode = new node(element);
+      let prevIndex = this.indexOf(prevElement);
+     
+      if(this.isEmpty()){throw "List is empty. Cannot Insert.";}
+      if(prevIndex === -1){throw `${prevElement} does not exist. Cannot insert new element after a non-existent element`;}
+      else{
+          //check if prevElement is at the end. Then insert last
+          if(this.tail.data === prevElement){
+              this.insertLast(element);
+          }
+          else{
+              let current = this.head;
+            while(current){
+                if(current.data === prevElement){
+                    ///insert
+                    newNode.prev = current;
+                    newNode.next = current.next;
+                    current.next.prev = newNode;
+                    current.next = newNode;
+                    this.length++;
+                    break;
+                }
+                else{
+                    current = current.next;
+                }
+            }
+          }
+         
+      }
+      
   }
+    
+    indexOf(element){
+        
+         // check if element exists in the list. return the index of first
+      // occurence
+      // return -1 if it does not exists
+
+      let current = this.head;
+      let index = -1;
+
+      while(current){
+        index++;
+        if(current.data === element){
+          return index;
+        }
+        current = current.next;
+      }
+      return -1; //not found
+    }
 
   remove(element){
-    //remove element form the list with given key
+    //remove first occurence of element from the list with given key
+       if(this.isEmpty()){
+      throw "List is empty. Cannot delete element of an empty list."
+    }
+      else{
+          let current = this.head;
+          
+          while(current){
+              if(current.data === element){
+                  //match
+                  current.prev.next = current.next;
+                  current.next.prev = current.prev;
+                  break;
+              }
+              else{
+                  current = current.next;
+              }
+          }
+          this.length--;
+      }
   }
 
   displayForward(){
@@ -96,6 +177,15 @@ class DLL {
 
   displayBackward(){
     //displays the list in backwards order tail to head
+      let current = this.tail;
+      console.log('NULL');
+      
+      while(current){
+          console.log(`<- ${current.data} ->`);
+          current = current.prev;
+      }
+      
+      console.log('NULL');
   }
 
 
@@ -113,10 +203,45 @@ class DLL {
 }
 //TEST
 
-try{
-  let dll = new DLL();
-  
-}
-catch(e){
-  console.log(e);
-}
+//try{
+//  let dll = new DLL();
+//    
+//    
+//    for ( let i = 1; i <6; i++){
+//        dll.insertFirst(i);
+//    }
+//       
+//    dll.displayForward();//5 - 4 - 3 -2 - 1
+//    dll.insertLast(6); //5 - 4 - 3 -2 - 1 - 6
+//    dll.deleteFirst(); // 4 - 3 - 2 - 1 - 6
+//    console.log(`Index of element 2: ${dll.indexOf(2)}`);//2
+//    console.log(`Size: ${dll.size()} \n`);//5
+//    
+//    
+//    dll.deleteLast(); // 4 - 3 - 2 -1 
+//    dll.displayBackward(); // 1 - 2 - 3 - 4
+//    
+//    console.log('\n');
+//    dll.insertAfter(4, 6); //1 - 2 - 3 - 6 - 4
+//    dll.insertAfter(6, 11); // 1 - 2 - 3 - 11 - 6 -4
+//    dll.displayForward(); // 4 - 6 - 11 - 3 -2 - 1
+//    
+//    console.log(`Size: ${dll.size()} \n`);//6
+//    
+//    console.log('\n');
+//    
+//    dll.remove(3); //4 - 6 - 11  - 2 - 1
+//    
+//    console.log(`Size: ${dll.size()} \n`);//5
+//    dll.insertLast(7); //4 - 6 - 11  - 2 - 1 - 7
+//    dll.displayBackward(); //7 - 1 - 2 - 11 - 6 - 4
+//    console.log(`Size: ${dll.size()} \n`);//6
+//    
+//    console.log(`Index of element 99: ${dll.indexOf(99)}`);//-1
+//   // dll.insertAfter(99,1);//does not exist
+//   
+//  
+//}
+//catch(e){
+//  console.log(e);
+//}
